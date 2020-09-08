4fae66fd35a63aa942e2268374c03b7aa28ff623
Cleionesio2020 <cleionesio@gmail.com>
2020-09-07 23:31:47 -0300

    comite tres

--- src/main/java/br/com/sis/beans/dao/DAO.java
+++ src/main/java/br/com/sis/beans/dao/DAO.java
@@ -5,7 +5,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DAO {
-private static final EntityManagerFactory emf =
-Persistence.createEntityManagerFactory("sistemas");
-public static EntityManager getEntityManager() { return emf.createEntityManager();
-} }
+
+    private static final EntityManagerFactory emf
+            = Persistence.createEntityManagerFactory("sistemas");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
